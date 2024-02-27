b22b27f2168f98fdebe19f111f084a2228995e80
Nicolas Ayral Seydoux <nseydoux@inrupt.com>
2024-02-27 15:59:42 +0100

    Add headers to ease debug

--- index.js
+++ index.js
@@ -2,10 +2,10 @@ import { createServer } from "node:http";
 
 const requestListener = function (req, res) {
     if (req.url === "/redirect") {
-        res.writeHead(302, { location: "/redirected", "Set-Cookie": "cookie" });
+        res.writeHead(302, { location: "/redirected", "Set-Cookie": "cookie", "x-custom-header": "coucou" });
         return res.end();
     }
-    res.writeHead(200);
+    res.writeHead(200, { "Content-type": "text/html" });
     res.write("<!DOCTYPE html>");
     res.write("<html><body><p data-testid='hello'>")
     res.write(
